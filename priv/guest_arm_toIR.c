dd3397efa6654c27bcca772b5cd832c6d5cd2caa
Fish Wang <fishw@asu.edu>
2026-07-29 12:20:47 +0000

    arm: lift any svc #imm as a syscall (OABI support)
    Port of fork commit a6576a1 (subsumes c958680 and 6955cf7, which it
    superseded). ARM svc imm24 and Thumb svc imm8 are lifted as
    Ijk_Sys_syscall regardless of the immediate, since OABI encodes the
    syscall number in the immediate. Note 3.27.1's ARM-mode condition had
    become 'imm24 == 0' rather than the fork base's form; both replaced
    with an unconditional block, matching fork HEAD.

--- priv/guest_arm_toIR.c
+++ priv/guest_arm_toIR.c
@@ -17439,7 +17439,7 @@ DisResult disInstr_ARM_WRK (
    /* --------------------- Svc --------------------- */
    if (BITS8(1,1,1,1,0,0,0,0) == (INSN(27,20) & BITS8(1,1,1,1,0,0,0,0))) {
       UInt imm24 = (insn >> 0) & 0xFFFFFF;
-      if (imm24 == 0) {
+      {
          /* A syscall.  We can't do this conditionally, hence: */
          if (condT != IRTemp_INVALID) {
             mk_skip_over_A32_if_cond_is_false( condT );
@@ -19771,7 +19771,7 @@ DisResult disInstr_THUMB_WRK (
    case BITS8(1,1,0,1,1,1,1,1): {
       /* ---------------- SVC ---------------- */
       UInt imm8 = INSN0(7,0);
-      if (imm8 == 0) {
+      {
          /* A syscall.  We can't do this conditionally, hence: */
          mk_skip_over_T16_if_cond_is_false( condT );
          // FIXME: what if we have to back up and restart this insn?
